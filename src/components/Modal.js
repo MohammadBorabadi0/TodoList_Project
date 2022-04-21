2717719b366ce52a21a3037f231b66da97e9de82
MohammadBorabadi0 <WWW.MohammadBorabadi0@gmail.com>
2022-04-21 17:51:51 +0430

    add Modal To Project And Complete EditTasks

---
+++ src/components/Modal.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+// Icons
+import { IoMdClose } from "react-icons/io";
+import { EDIT_TASK } from "../actions";
+import { useTodo } from "../Providers/context/todo_context";
+
+const Modal = ({ item, setIsEditing }) => {
+  const [editVal, setEditVal] = useState(item.name);
+  const { dispatch } = useTodo();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    if (editVal.trim() === "") {
+      alert("تسک نباید خالی باشد !!!");
+      return;
+    }
+
+    dispatch({ type: EDIT_TASK, payload: editVal, id: item.id });
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="fixed inset-0 bg-slate-900 flex justify-center items-center z-10">
+      <form
+        onSubmit={handleEdit}
+        className="relative w-[90%] md:w-[50%] bg-white h-[40vh] flex justify-center items-center"
+      >
+        <input
+          className="border-2 border-purple-900 focus:outline-none ml-[-3px] px-4 py-2 rounded-sm"
+          type="text"
+          value={editVal}
+          onChange={(e) => setEditVal(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="border-2 border-purple-900 bg-purple-900 px-4 py-2 rounded-sm text-white"
+        >
+          ویرایش
+        </button>
+        <button
+          className="absolute top-2 right-3 text-xl hover:bg-red-700 hover:text-white rounded-sm transition-all duration-200"
+          onClick={() => setIsEditing(false)}
+        >
+          <IoMdClose />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
+// Icons 
 import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
 
+import { REMOVE_TASK, TOGGLE_TASK } from "../actions";
+import { useTodo } from "../Providers/context/todo_context";
+
+// Components 
+import Modal from "./Modal";
+
+
+
 const TodoItem = ({ item }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+
+  const { dispatch } = useTodo();
+  const handleToggle = () => {
+    dispatch({ type: TOGGLE_TASK, id: item.id });
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  if (isEditing) {
+    return <Modal setIsEditing={setIsEditing} item={item} />;
+  }
+
   return (
-    <div className="flex justify-between items-center w-full bg-purple-200 p-4 mb-3 rounded-sm">
-      <div>
-        <h4>{item.name}</h4>
+    <div
+      className={`${
+        item.isComplete ? "opacity-50" : ""
+      } flex justify-between items-center w-full bg-purple-200 p-4 mb-3 rounded-sm`}
+    >
+      <div className="w-full">
+        <h4
+          className={`${item.isComplete ? "line-through" : ""} flex-1`}
+          onClick={handleToggle}
+        >
+          {item.name}
+        </h4>
       </div>
       <div className="flex items-center">
-        <button className="ml-2 p-1 bg-gray-300 text-orange-700 text-lg">
+        <button
+          className="ml-2 p-1 bg-gray-300 text-orange-700 text-lg"
+          onClick={handleEdit}
+        >
           <FaRegEdit />
         </button>
-        <button className="p-1 bg-gray-300 text-red-700 text-lg">
+        <button
+          className="p-1 bg-gray-300 text-red-700 text-lg"
+          onClick={() => dispatch({ type: REMOVE_TASK, payload: item.id })}
+        >
           <FaTrashAlt />
         </button>
       </div>
